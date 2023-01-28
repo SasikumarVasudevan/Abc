d40d0f8bc737e8bcab2b81854073e2c7df314e51
SasikumarVasudevan <sasi003kumar@gmail.com>
2023-01-28 09:19:13 +0530

    message

---
+++ src/org/abc/Employee.java
@@ -0,0 +1,19 @@
+package org.abc;
+
+public class Employee {
+	private void employeeName() {
+		System.out.println("sasi");
+
+	}
+	private void employeeId() {
+
+    System.out.println("1234");
+
+	}
+	public static void main(String[] args) {
+		Employee e =new Employee();
+		e.employeeName();
+		e.employeeId();
+	}
+
+}
